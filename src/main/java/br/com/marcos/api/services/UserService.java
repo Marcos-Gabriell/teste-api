0cde5b1ffe3ca0baaa492d50b8a0a3c31e9b9b5e
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-24 11:49:16 -0300

    Método create

--- src/main/java/br/com/marcos/api/services/UserService.java
+++ src/main/java/br/com/marcos/api/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.marcos.api.services;
 
 import br.com.marcos.api.domain.User;
+import br.com.marcos.api.domain.dto.UserDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,4 +11,5 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
--- src/main/java/br/com/marcos/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/marcos/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.marcos.api.services.impl;
 
 import br.com.marcos.api.domain.User;
+import br.com.marcos.api.domain.dto.UserDTO;
 import br.com.marcos.api.repositories.UserRepository;
 import br.com.marcos.api.services.UserService;
 import br.com.marcos.api.services.exeptions.ObjectFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
@@ -24,5 +29,10 @@ public class UserServiceImpl implements UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    @Override
+    public User create(UserDTO obj) {
+        return repository.save(mapper.map(obj, User.class)) ;
     }
 }
