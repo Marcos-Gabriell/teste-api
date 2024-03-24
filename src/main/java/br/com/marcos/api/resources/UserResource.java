b17c2c9b70d5b42b27fb63c92a791c359795233d
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-24 17:03:50 -0300

    Método delete

--- src/main/java/br/com/marcos/api/resources/UserResource.java
+++ src/main/java/br/com/marcos/api/resources/UserResource.java
@@ -51,4 +51,10 @@ public class  UserResource {
         User newObj = service.update(obj);
         return ResponseEntity.ok().body(mapper.map(newObj, UserDTO.class));
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
