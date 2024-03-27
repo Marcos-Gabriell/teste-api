a08e1601ef6d80c0d0d391a2979f6905b39ee7eb
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-27 12:21:10 -0300

    Teste do update correção

--- src/test/java/br/com/marcos/api/resources/UserResourceTest.java
+++ src/test/java/br/com/marcos/api/resources/UserResourceTest.java
@@ -109,6 +109,12 @@ class UserResourceTest {
         assertNotNull(response);
         assertNotNull(response.getBody());
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
     }
 
     @Test
