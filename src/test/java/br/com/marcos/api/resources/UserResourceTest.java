5e378bba5091d5ff89f6c8dee72d57029945c7cf
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-03-27 10:14:01 -0300

    Teste do findById

--- src/test/java/br/com/marcos/api/resources/UserResourceTest.java
+++ src/test/java/br/com/marcos/api/resources/UserResourceTest.java
@@ -1,18 +1,66 @@
 package br.com.marcos.api.resources;
 
+import br.com.marcos.api.domain.User;
+import br.com.marcos.api.domain.dto.UserDTO;
+import br.com.marcos.api.services.impl.UserServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
+@SpringBootTest
 class UserResourceTest {
+
+    private static final Integer ID      = 1;
+    private static final Integer INDEX   = 0;
+    private static final String NAME     = "Marcos";
+    private static final String EMAIL    = "marcos@mail.com";
+    private static final String PASSWORD = "123";
+
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @Mock
+    private ModelMapper mapper;
 
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
     }
 
     @Test
-    void findById() {
+    void whenFindByIdThenReturnSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
     @Test
@@ -29,5 +77,10 @@ class UserResourceTest {
 
     @Test
     void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL,  PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
     }
 }
